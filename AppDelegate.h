634eae4e1fa785253ac0ca504b4ca99dca608564
Misterxof <mishail.lukushevich@gmail.com>
2017-05-27 23:18:50 +0300

    1 commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tp8.2
+//
+//  Created by Admin on 20.05.17.
+//  Copyright (c) 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
